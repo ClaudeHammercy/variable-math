42f55fa8070711cd0fc49f09e83db034031e7935
Rashad Gant <83968751+rashadgant@users.noreply.github.com>
2021-09-20 15:28:25 -0400

    Quadratic

---
+++ quadratic.js
@@ -0,0 +1,6 @@
+const a = 71;
+const b = 1;
+const c = 22;
+
+console.log('positive', -1 * b + (b ** 2) - (4 * a * c) ** 0.5)/(2 * a);
+console.log('negative', -1 * b - (b ** 2) - (4 * a * c) ** 0.5)/(2 * a);
